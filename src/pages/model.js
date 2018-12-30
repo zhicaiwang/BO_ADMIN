b7bf57a92362e29009649bc51837da6c40aa5c24
顾云翔 <guyunxiang32@gmail.com>
2018-12-31 00:11:52 +0800

    aa

--- src/pages/model.js
+++ src/pages/model.js
@@ -9,28 +9,28 @@ import {
 } from '../utils';
 
 // 合约地址
-const contractAddress = 'TM9xckCzJuZEznHNhED4yfzQzTFXr5L5tu';
+const contractAddress = 'TMFTUYpBJw9rh6CnUzKd3b7Tvi1BUes42z';
 
 export default {
   namespace: 'home',
   state: {
     gameId: getGameId(),
-    address: '',
-    totalAmount: 0,
-    upPoolAmount: 0,
-    downPoolAmount: 0,
-    upBettersCount: 0,
-    downBettersCount: 0,
     result: 0,
     contract: null,
   },
   subscriptions: {
     steup({ dispatch, history }) {
-      history.listen(location => {
-        if (location.pathname === '/') {
-          dispatch({ type: 'getContract' });
+      window.addEventListener('load', function() {
+        if (typeof tronPay !== 'undefined') {
+          tronWeb = tronPay.tronWeb || tronWeb
+          if (tronWeb.isTronPay && tronWeb.ready) {
+            console.log(27);
+            dispatch({ type: 'getContract' });
+          }
+        } else {
+          console.log('No tronWeb? You should install TronPay!')
         }
-      });
+      })
     },
   },
   effects: {
@@ -38,6 +38,7 @@ export default {
       const contractData = yield call(getContractServer, contractAddress);
       if (contractData) {
         yield put({ type: 'setContract', payload: contractData });
+        console.log(contractData);
       }
     },
     *addGame(_, { call, put, select }) {
@@ -45,21 +46,15 @@ export default {
       const gameId = getGameId();
       const startTime = getStartTime();
       const endTime = getEndTime();
-      // console.log(contract, gameId, startTime, endTime);
+      console.log(contract, gameId, startTime, endTime);
       contract.addGame(gameId, startTime, endTime).send({
       }).then(res => {
         console.log(res);
+        alert('发布成功！');
       }).catch(err => {
         console.error(err);
       }).then(() => {
       });
-      contract.getUpAmount(gameId).call({
-
-      }).then((res) => {
-        console.log(res);
-      }).catch(err => {
-        console.log(err);
-      })
     }
   },
   reducers: {
